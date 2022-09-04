2a477a1b0ef49fbdf103559df6495611a8de05c6
Ander Dorado <adb35@alu.ua.es>
2022-09-04 17:23:19 +0200

    Estilo grid updated

--- front-app/src/components/Principal.js
+++ front-app/src/components/Principal.js
@@ -3,7 +3,7 @@ import '../styles/Principal.css';
 function Principal(props) {
   return (
     <div className="principal-element">
-      <h2 id="principal-title">{props.principalTitle}</h2>
+      <h2 id="principal-title">{props.principalTitle}: OFF</h2>
       <div id="principal-row-2">{props.children[0]}</div>
       <img id="principal-image" src={props.image} />
       <div id="principal-row-4">{props.children[1]}</div>
